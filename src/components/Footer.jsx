88e57ce1bfd9328a7975d4646f82a75d1b9c39cd
Andrew Chough <andrewchough@gmail.com>
2020-06-13 19:44:21 -0700

    Hopefully no errors

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,34 +1,31 @@
 import React from 'react'
 import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa'
-import { Link, BrowserRouter as Router } from 'react-router-dom'
 
 export default function Footer () {
   return (
-    <Router>
-      <div className="grid grid-cols-1 items-center block__section border-black border-solid">
-        <div className="mx-auto mt-8 mb-3 md:hidden">
-          <Link
-            to="/Andrew_Chough_Resume.pdf"
-            target="_blank" rel="noopener noreferrer"
-            download
-            className="bg-purple-500 text-white py-2 px-8 round-15 w-32 navbar__resume-button focus:outline-none focus:shadow-outline">RESUME
-          </Link>
-        </div>
-        <div className="m-auto">
-          <a href="https://github.com/andrewchough" target="_blank" rel="noopener noreferrer">
-            <FaGithub className="margin-auto inline my-6 mx-4 text-purple-600 footer__item" />
-          </a>
-          <a href="https://www.linkedin.com/in/andrewchough/" target="_blank" rel="noopener noreferrer">
-            <FaLinkedin className="margin-auto inline my-6 mx-4 text-purple-600 footer__item" />
-          </a>
-          <a href="mailto:andrewchough@gmail.com" target="_blank" rel="noopener noreferrer">
-            <FaEnvelope className="margin-auto inline my-6 mx-4 text-purple-600 footer__item" />
-          </a>
-        </div>
-        <div className="mx-auto my-4">
-          <p className="font-thin text-xs footer">COPYRIGHT © 2020 ANDREW CHOUGH</p>
-        </div>
+    <div className="grid grid-cols-1 items-center block__section border-black border-solid">
+      <div className="mx-auto mt-8 mb-3 md:hidden">
+        <a
+          target="_blank" rel="noopener noreferrer"
+          href="https://drive.google.com/file/d/1ALlfH1E9uvWa0417cLdCJE5AOmXwkXpR/view?usp=sharing"
+          className="bg-purple-500 text-white py-2 px-8 round-15 w-32 navbar__resume-button focus:outline-none focus:shadow-outline">
+          RESUME
+        </a>
       </div>
-    </Router>
+      <div className="m-auto">
+        <a href="https://github.com/andrewchough" target="_blank" rel="noopener noreferrer">
+          <FaGithub className="margin-auto inline my-6 mx-4 text-purple-600 footer__item" />
+        </a>
+        <a href="https://www.linkedin.com/in/andrewchough/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="margin-auto inline my-6 mx-4 text-purple-600 footer__item" />
+        </a>
+        <a href="mailto:andrewchough@gmail.com" target="_blank" rel="noopener noreferrer">
+          <FaEnvelope className="margin-auto inline my-6 mx-4 text-purple-600 footer__item" />
+        </a>
+      </div>
+      <div className="mx-auto my-4">
+        <p className="font-thin text-xs footer">COPYRIGHT © 2020 ANDREW CHOUGH</p>
+      </div>
+    </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,19 @@
 import React from 'react'
-// import { Link } from 'gatsby'
-import { Link, BrowserRouter as Router } from 'react-router-dom'
 
 export default function Navbar () {
   return (
-    <Router>
-      <div className="flex items-center justify-between flex-wrap p-5 bg-purple-700 navbar__container">
-        <div className="flex items-center flex-shrink-0 navbar__name__container">
-          <span className="font-medium text-2xl tracking-tight text-white lg:ml-5 navbar__name">ANDREW CHOUGH</span>
-        </div>
-        <div className="float-right">
-          {/* <Link className="font-medium text-l tracking-tight text-white mr-12 hover:underline navbar__contact-link" to="/contact">CONTACT</Link> */}
-          <Link
-            to="/Andrew_Chough_Resume.pdf"
-            target="_blank" rel="noopener noreferrer"
-            download
-            className="bg-purple-500 text-white py-2 px-8 round-15 w-32 navbar__resume-button focus:outline-none focus:shadow-outline md-max:hidden">RESUME
-          </Link>
-        </div>
+    <div className="flex items-center justify-between flex-wrap p-5 bg-purple-700 navbar__container">
+      <div className="flex items-center flex-shrink-0 navbar__name__container">
+        <span className="font-medium text-2xl tracking-tight text-white lg:ml-5 navbar__name">ANDREW CHOUGH</span>
       </div>
-    </Router>
+      <div className="float-right">
+        <a
+          target="_blank" rel="noopener noreferrer"
+          href="https://drive.google.com/file/d/1ALlfH1E9uvWa0417cLdCJE5AOmXwkXpR/view?usp=sharing"
+          className="bg-purple-500 text-white py-2 px-8 round-15 w-32 navbar__resume-button focus:outline-none focus:shadow-outline md-max:hidden">
+          RESUME
+        </a>
+      </div>
+    </div>
   )
 }
