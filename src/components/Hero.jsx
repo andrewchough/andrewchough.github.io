2d81a1275463546451f0497faf7f984853c182eb
Andrew Chough <achough@adobe.com>
2023-07-15 16:46:57 -0400

    Add animation library + one cool animation 8)

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Fade, Slide } from "react-awesome-reveal";
 
 import { Button, Divider, Flex, Grid, MediaQuery, Text } from "@mantine/core";
 import { IconBrandGithub, IconBrandLinkedin } from "@tabler/icons";
@@ -23,61 +24,65 @@ export default function Hero() {
       >
         <MediaQuery smallerThan="lg" styles={{ marginTop: "48px" }}>
           <Grid.Col sm={12} orderXs={2} lg={7} orderLg={1}>
-            <Text size="xl">
+            <Slide triggerOnce>
+              <Text size="xl">
               Andrew is a <span className="text-dark-blue">UX designer</span>
-              {" and "}
-              <span className="text-dark-green">frontend engineer</span> who
+                {" and "}
+                <span className="text-dark-green">frontend engineer</span> who
               values inclusivity and empathy in creating products that generate
               meaningful change in people's lives and is enthusiastic about opportunities to influence an end-to-end experience.
-            </Text>
-            <Divider my={40} size="sm" />
-            <Text size="lg">
+              </Text>
+              <Divider my={40} size="sm" />
+              <Text size="lg">
               Currently stationed @ <b>Adobe</b> in 🗽
-            </Text>
-            <Flex mt={16}>
-              <Button
-                component="a"
-                size="sm"
-                leftIcon={<IconBrandGithub size={32} />}
-                variant="subtle"
-                color="white"
-                href="https://github.com/andrewchough"
-                target="_blank"
-                rel="noreferrer"
-                styles={() => ({
-                  root: {
-                    color: "white",
-                    paddingRight: 0,
-                    "&:hover": {
-                      backgroundColor: "rgba(255, 255, 255, 0.2)"
-                    },
-                    marginRight: 4,
-                    height: 48
-                  }
-                })}
-              />
-              <Button
-                component="a"
-                size="sm"
-                leftIcon={<IconBrandLinkedin size={36} />}
-                variant="subtle"
-                color="white"
-                href="https://www.linkedin.com/in/andrewchough"
-                target="_blank"
-                rel="noreferrer"
-                styles={() => ({
-                  root: {
-                    color: "white",
-                    paddingRight: 0,
-                    "&:hover": {
-                      backgroundColor: "rgba(255, 255, 255, 0.2)"
-                    },
-                    marginRight: 4,
-                    height: 48
-                  }
-                })}
-              />
-            </Flex>
+              </Text>
+            </Slide>
+            <Fade duration="4000" triggerOnce>
+              <Flex mt={16}>
+                <Button
+                  component="a"
+                  size="sm"
+                  leftIcon={<IconBrandGithub size={32} />}
+                  variant="subtle"
+                  color="white"
+                  href="https://github.com/andrewchough"
+                  target="_blank"
+                  rel="noreferrer"
+                  styles={() => ({
+                    root: {
+                      color: "white",
+                      paddingRight: 0,
+                      "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 0.2)"
+                      },
+                      marginRight: 4,
+                      height: 48
+                    }
+                  })}
+                />
+                <Button
+                  component="a"
+                  size="sm"
+                  leftIcon={<IconBrandLinkedin size={36} />}
+                  variant="subtle"
+                  color="white"
+                  href="https://www.linkedin.com/in/andrewchough"
+                  target="_blank"
+                  rel="noreferrer"
+                  styles={() => ({
+                    root: {
+                      color: "white",
+                      paddingRight: 0,
+                      "&:hover": {
+                        backgroundColor: "rgba(255, 255, 255, 0.2)"
+                      },
+                      marginRight: 4,
+                      height: 48
+                    }
+                  })}
+                />
+              </Flex>
+            </Fade>
           </Grid.Col>
         </MediaQuery>
         <Grid.Col
@@ -88,13 +93,16 @@ export default function Hero() {
           orderLg={2}
           className="hero__image-container"
         >
-          <Image
-            src={ProfilePicture}
-            alt="Picture of Andrew Chough"
-            className="hero__image"
-            priority
-          />
+          <Fade duration="2000" triggerOnce>
+            <Image
+              src={ProfilePicture}
+              alt="Picture of Andrew Chough"
+              className="hero__image"
+              priority
+            />
+          </Fade>
         </Grid.Col>
+
       </Grid>
     </div>
   );
